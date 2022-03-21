3f6b17642fe7d25bed689a0c135bc38e756b29de
Rayane Tayache <82381064+tayacherayane@users.noreply.github.com>
2022-03-22 00:14:56 +0100

    Create departHighestSalary.sql

---
+++ departHighestSalary.sql
@@ -0,0 +1,7 @@
+#184. Department Highest Salary
+
+Select Distinct d.name as Department, 
+                e.name as Employee, 
+                e.salary as Salary 
+                FROM employee e, department d, (select max(e.salary) as maxSalary, e.departmentId as departmentId from employee e group by e.departmentId) s 
+                WHERE e.departmentId = d.id and e.salary= s.maxSalary and e.departmentId = s.departmentId;
