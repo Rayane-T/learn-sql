b45c95668ee7b055fc7b7323bef4cb4dfc8d2bcd
Rayane Tayache <82381064+tayacherayane@users.noreply.github.com>
2022-03-26 01:22:27 +0100

    Update departHighestSalary.sql

--- departHighestSalary.sql
+++ departHighestSalary.sql
@@ -3,5 +3,6 @@
 Select Distinct d.name as Department, 
                 e.name as Employee, 
                 e.salary as Salary 
-                FROM employee e, department d, (select max(e.salary) as maxSalary, e.departmentId as departmentId from employee e group by e.departmentId) s 
-                WHERE e.departmentId = d.id and e.salary= s.maxSalary and e.departmentId = s.departmentId;
+                FROM employee e, department d, (select max(e.salary) as maxSalary, e.departmentId as departmentId 
+                                                from employee e group by e.departmentId) s 
+                                                WHERE e.departmentId = d.id and e.salary= s.maxSalary and e.departmentId = s.departmentId;
